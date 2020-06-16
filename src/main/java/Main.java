f6e8eb640e37c4d1fba4473e8adffd3e865ee456
evgsavelov <savelov08@ya.ru>
2020-06-16 20:32:26 +0300

    fixes #1 Correct_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,17 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
+    //задаем ожидаемое значение
+    long expected = 30;
 
-    service.calculate(amount, registered);
+    //вызываем целевой метод
+    long actual = service.calculate(amount, registered);
+
+    //сравниваем ожидаемый и фактический результат
+    boolean passed = expected == actual;
+
+    //выводим результат
+    System.out.println(passed);
   }
 }
 
